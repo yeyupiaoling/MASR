965489eb56c1807b6d5c48e37286e085e06557ca
yeyupiaoling <yeyupiaoling@foxmail.com>
2020-07-31 10:58:50 +0800

    finish

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torch.nn as nn
 from models.conv import GatedConv
@@ -29,10 +30,6 @@ parser.add_argument("--save_model_path",
                     default="save_model/",
                     type=str,
                     help="vocab file path. (default: %(default)s)")
-parser.add_argument("--device_ids",
-                    default="0,1",
-                    type=str,
-                    help="use GPU number. (default: %(default)s)")
 parser.add_argument("--epochs",
                     default=1000,
                     type=int,
@@ -57,9 +54,9 @@ def train(model,
     train_dataset = data.MASRDataset(train_manifest_path, vocab_path)
     batchs = (len(train_dataset) + batch_size - 1) // batch_size
     dev_dataset = data.MASRDataset(dev_manifest_path, vocab_path)
-    train_dataloader = data.MASRDataLoader(train_dataset, batch_size=batch_size * len(device_ids), num_workers=8)
-    train_dataloader_shuffle = data.MASRDataLoader(train_dataset, batch_size=batch_size * len(device_ids), num_workers=8, shuffle=True)
-    dev_dataloader = data.MASRDataLoader(dev_dataset, batch_size=batch_size * len(device_ids), num_workers=8)
+    train_dataloader = data.MASRDataLoader(train_dataset, batch_size=batch_size, num_workers=8)
+    train_dataloader_shuffle = data.MASRDataLoader(train_dataset, batch_size=batch_size, num_workers=8, shuffle=True)
+    dev_dataloader = data.MASRDataLoader(dev_dataset, batch_size=batch_size, num_workers=8)
     parameters = model.parameters()
     optimizer = torch.optim.SGD(parameters,
                                 lr=learning_rate,
@@ -78,7 +75,7 @@ def train(model,
         lr = get_lr(optimizer)
         writer.add_scalar("lr/epoch", lr, epoch)
         for i, (x, y, x_lens, y_lens) in enumerate(train_dataloader):
-            x = x.cuda(device=device_ids[0])
+            x = x.cuda()
             out, out_lens = model(x, x_lens)
             out = out.transpose(0, 1).transpose(0, 2)
             loss = ctcloss(out, y, out_lens, y_lens)
@@ -95,7 +92,8 @@ def train(model,
         writer.add_scalar("loss/epoch", epoch_loss, epoch)
         writer.add_scalar("cer/epoch", cer, epoch)
         print("Epoch {}: Loss= {}, CER = {}".format(epoch, epoch_loss, cer))
-        torch.save(model, "{}/model_{}.pth".format(args.save_model_path, epoch))
+        torch.save(model, os.path.join(args.save_model_path, "model_{}.pt".format(epoch)),
+                   _use_new_zipfile_serialization=False)
 
 
 def get_lr(optimizer):
@@ -110,7 +108,7 @@ def evaluate(model, dataloader):
     print("decoding")
     with torch.no_grad():
         for i, (x, y, x_lens, y_lens) in tqdm(enumerate(dataloader)):
-            x = x.cuda(device=device_ids[0])
+            x = x.cuda()
             outs, out_lens = model(x, x_lens)
             outs = F.softmax(outs, 1)
             outs = outs.transpose(1, 2)
@@ -131,12 +129,13 @@ def evaluate(model, dataloader):
 
 def main():
     print_arguments(args)
+    if not os.path.exists(args.save_model_path):
+        os.makedirs(args.save_model_path)
     with open(args.vocab_path, 'r', encoding='utf-8') as f:
         vocabulary = eval(f.read())
         vocabulary = "".join(vocabulary)
     model = GatedConv(vocabulary)
-    model = torch.nn.DataParallel(model, device_ids=device_ids)  # 声明所有可用设备
-    model = model.cuda(device=device_ids[0])  # 模型放在主设备
+    model = model.cuda()
     train(model=model,
           train_manifest_path=args.train_manifest_path,
           dev_manifest_path=args.dev_manifest_path,
@@ -146,5 +145,4 @@ def main():
 
 
 if __name__ == "__main__":
-    device_ids = [int(i) for i in str(args.device_ids).split(',')]
     main()
