d4fa6b50a36b2e4525ec55b35515163552b89164
dxl <dxl@britao.com>
2022-08-15 11:18:21 +0800

    temporary default of export

--- export_model.py
+++ export_model.py
@@ -6,12 +6,12 @@ from masr.utils.utils import add_arguments, print_arguments
 
 parser = argparse.ArgumentParser(description=__doc__)
 add_arg = functools.partial(add_arguments, argparser=parser)
-add_arg('use_model',        str,   'deepspeech2_big',              '所使用的模型', choices=['deepspeech2', 'deepspeech2_big'])
+add_arg('use_model',        str,   'deepspeech2',              '所使用的模型', choices=['deepspeech2', 'deepspeech2_big'])
 add_arg('dataset_vocab',    str,   'dataset/vocabulary.txt',   '数据字典的路径')
 add_arg('mean_std_path',    str,   'dataset/mean_std.npz',     '数据集的均值和标准值的npy文件路径')
 add_arg('save_model',       str,   'models/',                  '模型保存的路径')
 add_arg('feature_method',   str,   'linear',                   '音频预处理方法', choices=['linear', 'mfcc', 'fbank'])
-add_arg('resume_model',     str,   'models/deepspeech2_big/best_model/',  '准备转换的模型路径')
+add_arg('resume_model',     str,   'models/deepspeech2/best_model/',  '准备转换的模型路径')
 args = parser.parse_args()
 print_arguments(args)
 
