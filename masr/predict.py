0b9f79d6fa1c15053194f53b1f34e658898a417c
yeyupiaoling <yeyupiaoling@foxmail.com>
2023-03-22 19:47:02 +0800

    修改预测加载音频方式

--- masr/predict.py
+++ masr/predict.py
@@ -1,5 +1,6 @@
 import os
 import platform
+from io import BufferedReader
 
 import cn2an
 import numpy as np
@@ -138,6 +139,26 @@ class MASRPredictor:
             text = self.inverse_text_normalization(text)
         return score, text
 
+    @staticmethod
+    def _load_audio(audio_data, sample_rate=16000):
+        """加载音频
+        :param audio_data: 需要识别的数据，支持文件路径，文件对象，字节，numpy。如果是字节的话，必须是完整的字节文件
+        :param sample_rate: 如果传入的事numpy数据，需要指定采样率
+        :return: 识别的文本结果和解码的得分数
+        """
+        # 加载音频文件，并进行预处理
+        if isinstance(audio_data, str):
+            audio_segment = AudioSegment.from_file(audio_data)
+        elif isinstance(audio_data, BufferedReader):
+            audio_segment = AudioSegment.from_file(audio_data)
+        elif isinstance(audio_data, np.ndarray):
+            audio_segment = AudioSegment.from_ndarray(audio_data, sample_rate)
+        elif isinstance(audio_data, bytes):
+            audio_segment = AudioSegment.from_bytes(audio_data)
+        else:
+            raise Exception(f'不支持该数据类型，当前数据类型为：{type(audio_data)}')
+        return audio_segment
+
     # 预测音频
     def predict(self,
                 audio_data,
@@ -146,21 +167,14 @@ class MASRPredictor:
                 sample_rate=16000):
         """
         预测函数，只预测完整的一句话。
-        :param audio_data: 需要识别的数据，支持文件路径，字节，numpy。如果是字节的话，必须是完整的字节文件
+        :param audio_data: 需要识别的数据，支持文件路径，文件对象，字节，numpy。如果是字节的话，必须是完整的字节文件
         :param use_pun: 是否使用加标点符号的模型
         :param is_itn: 是否对文本进行反标准化
         :param sample_rate: 如果传入的事numpy数据，需要指定采样率
         :return: 识别的文本结果和解码的得分数
         """
         # 加载音频文件，并进行预处理
-        if isinstance(audio_data, str):
-            audio_segment = AudioSegment.from_file(audio_data)
-        elif isinstance(audio_data, np.ndarray):
-            audio_segment = AudioSegment.from_ndarray(audio_data, sample_rate)
-        elif isinstance(audio_data, bytes):
-            audio_segment = AudioSegment.from_bytes(audio_data)
-        else:
-            raise Exception(f'不支持该数据类型，当前数据类型为：{type(audio_data)}')
+        audio_segment = self._load_audio(audio_data=audio_data, sample_rate=sample_rate)
         audio_feature = self._audio_featurizer.featurize(audio_segment)
         input_data = np.array(audio_feature).astype(np.float32)[np.newaxis, :]
         audio_len = np.array([input_data.shape[1]]).astype(np.int64)
@@ -181,7 +195,7 @@ class MASRPredictor:
                      sample_rate=16000):
         """
         预测函数，只预测完整的一句话。
-        :param audio_data: 需要识别的数据，支持文件路径，字节，numpy。如果是字节的话，必须是完整的字节文件
+        :param audio_data: 需要识别的数据，支持文件路径，文件对象，字节，numpy。如果是字节的话，必须是完整的字节文件
         :param use_pun: 是否使用加标点符号的模型
         :param is_itn: 是否对文本进行反标准化
         :param sample_rate: 如果传入的事numpy数据，需要指定采样率
@@ -191,14 +205,7 @@ class MASRPredictor:
             from masr.infer_utils.vad_predictor import VADPredictor
             self.vad_predictor = VADPredictor()
         # 加载音频文件，并进行预处理
-        if isinstance(audio_data, str):
-            audio_segment = AudioSegment.from_file(audio_data)
-        elif isinstance(audio_data, np.ndarray):
-            audio_segment = AudioSegment.from_ndarray(audio_data, sample_rate)
-        elif isinstance(audio_data, bytes):
-            audio_segment = AudioSegment.from_bytes(audio_data)
-        else:
-            raise Exception(f'不支持该数据类型，当前数据类型为：{type(audio_data)}')
+        audio_segment = self._load_audio(audio_data=audio_data, sample_rate=sample_rate)
         # 重采样，方便进行语音活动检测
         if audio_segment.sample_rate != self.configs.preprocess_conf.sample_rate:
             audio_segment.resample(self.configs.preprocess_conf.sample_rate)
